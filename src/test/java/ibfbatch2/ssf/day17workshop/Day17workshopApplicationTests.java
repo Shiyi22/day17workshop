4f04bc345a9a869a0e6a073f46f5724631b70adb
Shiyi <yeoshiyi22@gmail.com>
2023-02-28 14:33:17 +0800

    initial commit

---
+++ src/test/java/ibfbatch2/ssf/day17workshop/Day17workshopApplicationTests.java
@@ -0,0 +1,13 @@
+package ibfbatch2.ssf.day17workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day17workshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
